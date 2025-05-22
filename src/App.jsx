dd7fffd80744fca9b151f192d3fdb2ee967355c0
Maycon Gibson <maycon_g_santos@estudante.sesisenai.org.br>
2025-05-22 20:23:29 -0300

    Add files via upload
    ...

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Body from './components/Body'
+import DragonBall from './components/DragonBall'
+import Header from './components/Header'
+import Icon from './components/Icon'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Body />
+    <Icon />
+    </>
+  )
+}
+
+export default App
